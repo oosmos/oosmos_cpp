d07b391cbb24e6393b51097bb1531141b725e956
zzglenm <mglenn@cox.net>
2020-04-13 10:33:37 -0700

    remove superfluous 'OOSMOS::' from some method definitions.

--- oosmos.cpp
+++ oosmos.cpp
@@ -17,36 +17,36 @@ namespace OOSMOS {
     m_TimeoutUS = 0;
   }
 
-  void OOSMOS::cTimeout::TimeoutInUS(uint32_t TimeoutUS)
+  void cTimeout::TimeoutInUS(uint32_t TimeoutUS)
   {
     m_StartUS   = OS::GetFreeRunningUS();
     m_TimeoutUS = TimeoutUS;
   }
 
-  void OOSMOS::cTimeout::TimeoutInMS(uint32_t TimeoutMS)
+  void cTimeout::TimeoutInMS(uint32_t TimeoutMS)
   {
     TimeoutInUS(TimeoutMS * 1000);
   }
 
-  void OOSMOS::cTimeout::TimeoutInSeconds(uint32_t TimeoutSeconds)
+  void cTimeout::TimeoutInSeconds(uint32_t TimeoutSeconds)
   {
     TimeoutInMS(TimeoutSeconds * 1000);
   }
 
-  bool OOSMOS::cTimeout::HasExpired() const
+  bool cTimeout::HasExpired() const
   {
     const uint32_t NowUS = OS::GetFreeRunningUS();
 
     return (NowUS - m_StartUS) >= m_TimeoutUS;
   }
 
-  OOSMOS::cStack::cStack()
+  cStack::cStack()
   {
     m_ThreadContext = OOSMOS_THREAD_CONTEXT_BEGIN;
     m_FirstEntry    = true;
   }
 
-  bool OOSMOS::cStack::OOSMOS_ThreadDelayUS(uint32_t US)
+  bool cStack::OOSMOS_ThreadDelayUS(uint32_t US)
   {
     if (m_FirstEntry) {
       m_ThreadTimeout.TimeoutInUS(US);
@@ -62,17 +62,17 @@ namespace OOSMOS {
     return false;
   }
 
-  bool OOSMOS::cStack::OOSMOS_ThreadDelayMS(uint32_t MS)
+  bool cStack::OOSMOS_ThreadDelayMS(uint32_t MS)
   {
     return OOSMOS_ThreadDelayUS(MS * 1000);
   }
 
-  bool OOSMOS::cStack::OOSMOS_ThreadDelaySeconds(uint32_t Seconds)
+  bool cStack::OOSMOS_ThreadDelaySeconds(uint32_t Seconds)
   {
     return OOSMOS_ThreadDelayUS(Seconds * 1000 * 1000);
   }
 
-  bool OOSMOS::cStack::OOSMOS_ThreadWaitCond_TimeoutMS(bool Condition, uint32_t TimeoutMS, bool * pTimeoutStatus)
+  bool cStack::OOSMOS_ThreadWaitCond_TimeoutMS(bool Condition, uint32_t TimeoutMS, bool * pTimeoutStatus)
   {
     if (m_FirstEntry) {
       m_ThreadTimeout.TimeoutInMS(TimeoutMS);
@@ -95,7 +95,7 @@ namespace OOSMOS {
     return false;
   }
 
-  bool OOSMOS::cStack::OOSMOS_ThreadYield()
+  bool cStack::OOSMOS_ThreadYield()
   {
     if (m_FirstEntry) {
       m_FirstEntry = false;
